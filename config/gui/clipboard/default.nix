5a5d7bccbd82edfad4f8e3e5dc11d1c3720811db
fym998 <61316972+fym998@users.noreply.github.com>
2026-01-08 23:33:25 +0800

    clipboard: 重构，使用nixpkgs中的依赖

--- config/gui/clipboard/default.nix
+++ config/gui/clipboard/default.nix
@@ -6,34 +6,55 @@
 }:
 let
   shell = pkgs.stdenvNoCC.shell;
+  inherit (config.wayland.systemd) target;
 in
 {
-  systemd.user.services.clipboard-bridge-wl-to-x11 = {
-    Service = {
-      Type = "exec";
-      ExecStart = "wl-paste --watch ${shell} ${./bridge-wl-to-x11.sh}";
-    };
-    Unit = {
-      Description = "Bridge Wayland clipboard to X11";
-      Documentation = "https://github.com/bugaevc/wl-clipboard";
-      After = config.wayland.systemd.target;
-    };
-    Install = {
-      WantedBy = [ config.wayland.systemd.target ];
-    };
-  };
-  systemd.user.services.clipboard-bridge-x11-to-wl = {
-    Service = {
-      Type = "exec";
-      ExecStart = "${shell} ${./bridge-x11-to-wl.sh}";
-    };
-    Unit = {
-      Description = "Bridge X11 clipboard to Wayland";
-      Documentation = "https://github.com/bugaevc/wl-clipboard";
-      After = config.wayland.systemd.target;
-    };
-    Install = {
-      WantedBy = [ config.wayland.systemd.target ];
-    };
+  systemd.user.services = {
+    clipboard-bridge-wl-to-x11 =
+      let
+        deps = [
+          pkgs.wl-clipboard
+          pkgs.xclip
+        ];
+      in
+      {
+        Service = {
+          Type = "exec";
+          ExecStart = "wl-paste --watch ${shell} ${./bridge-wl-to-x11.sh}";
+          Environment = "PATH=${lib.makeBinPath deps}";
+        };
+        Unit = {
+          Description = "Bridge Wayland clipboard to X11";
+          Documentation = builtins.concatStringsSep " " (map (dep: dep.meta.homepage or "") deps);
+          After = target;
+        };
+        Install = {
+          WantedBy = [ target ];
+        };
+      };
+    clipboard-bridge-x11-to-wl =
+      let
+        deps = [
+          pkgs.wl-clipboard
+          pkgs.xclip
+          pkgs.clipnotify
+          pkgs.diffutils
+        ];
+      in
+      {
+        Service = {
+          Type = "exec";
+          ExecStart = "${shell} ${./bridge-x11-to-wl.sh}";
+          Environment = "PATH=${lib.makeBinPath deps}";
+        };
+        Unit = {
+          Description = "Bridge X11 clipboard to Wayland";
+          Documentation = builtins.concatStringsSep " " (map (dep: dep.meta.homepage or "") deps);
+          After = target;
+        };
+        Install = {
+          WantedBy = [ target ];
+        };
+      };
   };
 }
