5bc7a28475ccb576ab7e1a8d5171a46c543c3687
fym998 <61316972+fym998@users.noreply.github.com>
2026-01-12 15:16:54 +0800

    niri: fix dingtalk notification pos

---
+++ config/gui/niri/settings/apps/dingtalk.nix
@@ -0,0 +1,14 @@
+{
+  programs.niri.settings.window-rules = [
+    {
+      matches = [
+        {
+          title = "com.alibabainc.dingtalk";
+          app-id = "com.alibabainc.dingtalk";
+          is-floating = true;
+        }
+      ];
+      open-focused = false;
+    }
+  ];
+}
