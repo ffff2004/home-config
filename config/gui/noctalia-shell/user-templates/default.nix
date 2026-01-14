0a48d81a9c7bfcee9a12723d803a5ae555e79058
fym998 <61316972+fym998@users.noreply.github.com>
2026-01-14 14:43:19 +0800

    fk matugen

--- config/gui/noctalia-shell/user-templates/default.nix
+++ config/gui/noctalia-shell/user-templates/default.nix
@@ -6,11 +6,14 @@
   ...
 }:
 {
-  programs.noctalia-shell.user-templates.templates = {
-    pywalfox = {
-      input_path = localLib.mkSymlinkToSource ./pywalfox-update.sh;
-      output_path = "~/.cache/wal/pywalfox-update.sh";
-      post_hook = "sh ~/.cache/wal/pywalfox-update.sh";
+  programs.noctalia-shell.user-templates = {
+    config = { };
+    templates = {
+      pywalfox = {
+        input_path = localLib.mkSymlinkToSource ./pywalfox-update.sh;
+        output_path = "~/.cache/wal/pywalfox-update.sh";
+        post_hook = "sh ~/.cache/wal/pywalfox-update.sh";
+      };
     };
   };
   home.packages = lib.mkIf config.programs.noctalia-shell.enable [ pkgs.pywalfox-native ];
