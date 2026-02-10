9455a03a55f97c715d316a2edcd7449ee03ef010
fym998 <61316972+fym998@users.noreply.github.com>
2026-02-10 11:36:08 +0800

    gui/niri: fk hypergryph

--- config/gui/niri/settings/apps/arknights.nix
+++ config/gui/niri/settings/apps/arknights.nix
@@ -1,54 +1,66 @@
 {
-  programs.niri.settings = {
-    window-rules = [
-      {
-        matches = [
-          { app-id = "^maa.exe$"; }
-          { app-id = "^waydroid.com.hypergryph.arknights$"; }
-          { app-id = "^elysia$"; }
-          {
-            title = "^Endfield$";
-            app-id = "^steam_app_default$";
-          }
-        ];
-        open-on-workspace = "arknights";
-      }
-      {
-        matches = [
-          {
-            title = "^Form$";
-            app-id = "^steam_app_default$";
-            is-floating = true;
-          }
-        ];
-        open-floating = false;
-      }
-      {
-        matches = [
-          {
-            title = "^Form$";
-            app-id = "^steam_app_default$";
-            is-floating = false;
-          }
-        ];
-        open-floating = true;
-      }
-      {
-        matches = [
-          {
-            title = "^$";
-            app-id = "^steam_app_default$";
-            is-floating = true;
-          }
-          {
-            title = "^鹰角启动器$";
-            app-id = "^steam_app_default$";
-            is-floating = true;
-          }
-        ];
-        open-floating = false;
-      }
-    ];
-    workspaces.arknights = { };
-  };
+  programs.niri.settings =
+    let
+      workspace = "arknights";
+    in
+    {
+      window-rules = [
+        {
+          matches = [
+            { app-id = "^maa.exe$"; }
+            { app-id = "^waydroid.com.hypergryph.arknights$"; }
+          ];
+          open-on-workspace = workspace;
+        }
+        {
+          matches = [
+            {
+              title = "^Form$";
+              app-id = "^steam_app";
+              # is-floating = true;
+            }
+            {
+              title = "^Endfield$";
+              app-id = "^steam_app";
+            }
+          ];
+          open-floating = false;
+          open-on-workspace = workspace;
+        }
+
+        {
+          matches = [
+            {
+              title = "^EFTool$";
+              app-id = "^steam_app";
+              # is-floating = true;
+            }
+            {
+              title = "^其他选项$";
+              app-id = "^steam_app";
+              # is-floating = true;
+            }
+          ];
+          open-floating = false;
+          open-on-workspace = workspace;
+        }
+        {
+          matches = [
+            {
+              title = "^$";
+              app-id = "^steam_app";
+              # is-floating = true;
+            }
+            {
+              title = "鹰角启动器$";
+              app-id = "^steam_app";
+              # is-floating = true;
+            }
+          ];
+          open-floating = false;
+          open-on-workspace = workspace;
+        }
+      ];
+      workspaces.${workspace} = { };
+    };
 }
