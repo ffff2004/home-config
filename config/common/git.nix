120a9bdb41c3b861c75b38a2ef2ef03e5a6684fd
fym998 <61316972+fym998@users.noreply.github.com>
2026-04-16 02:41:05 +0800

    feat(common/git): add alias for cloning with filter and submodules

--- config/common/git.nix
+++ config/common/git.nix
@@ -13,6 +13,9 @@
         email = "61316972+fym998@users.noreply.github.com";
       };
       init.defaultBranch = "main";
+      alias = {
+        cl = "clone --filter=blob:none --recurse-submodules --also-filter-submodules";
+      };
       # https://forums.whonix.org/t/git-users-enable-fsck-by-default-for-better-security/2066
       transfer.fsckobjects = true;
       fetch.fsckobjects = true;
