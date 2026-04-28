6552df35731ea7e068bdbb7b40dc13c3e992424c
fym998 <61316972+fym998@users.noreply.github.com>
2026-04-28 11:43:21 +0800

    feat(neovim): add additional plugins: - bufferline-nvim - neo-tree-nvim - toggleterm-nvim

--- config/common/neovim.nix
+++ config/common/neovim.nix
@@ -6,6 +6,38 @@
     extraConfig = ''
       set clipboard=unnamedplus
     '';
+
+    plugins = with pkgs.vimPlugins; [
+      plenary-nvim
+      nui-nvim
+      nvim-web-devicons
+      {
+        plugin = bufferline-nvim;
+        type = "lua";
+        config = ''
+          vim.opt.termguicolors = true
+          require("bufferline").setup({})
+        '';
+      }
+      {
+        plugin = neo-tree-nvim;
+        type = "lua";
+        config = ''
+          vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+        '';
+      }
+      {
+        plugin = toggleterm-nvim;
+        type = "lua";
+        config = ''
+          require("toggleterm").setup({
+            open_mapping = [[<C-`>]],
+            start_in_insert = true,
+            direction = 'horizontal'
+          })
+        '';
+      }
+    ];
     coc = {
       enable = true;
       settings = {
