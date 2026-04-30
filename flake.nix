af7d2ee7223e9a698e2fd60116fea25520b72dd6
fym998 <61316972+fym998@users.noreply.github.com>
2026-04-30 12:37:22 +0800

    refactor: restructure flake configuration: add nix and home-manager settings

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-{
+rec {
   description = "Home Manager configuration of fym";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -28,6 +28,26 @@
       url = "github:gpakosz/.tmux";
       flake = false;
     };
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+      "https://noctalia.cachix.org"
+      "https://fym998-nur.cachix.org"
+      "https://pre-commit-hooks.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "fym998-nur.cachix.org-1:lWwztkEXGJsiJHh/5FbA2u95AxJu8/k4udgGqdFLhOU="
+      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
   };
 
   outputs =
@@ -87,6 +107,30 @@
 
           {
             home = { inherit username homeDirectory; };
+            home.stateVersion = "26.05"; # Do not change!
+
+            programs.home-manager.enable = true;
+            manual = {
+              html.enable = true;
+              json.enable = true;
+              manpages.enable = true;
+            };
+          }
+
+          {
+            nix =
+              let
+                flakes = {
+                  home-config = self;
+                }
+                // builtins.removeAttrs inputs [ "self" ];
+              in
+              {
+                registry = builtins.mapAttrs (_: flake: { inherit flake; }) flakes;
+                channels = flakes;
+                package = pkgs.nix;
+                settings = nixConfig;
+              };
           }
         ];
       };
