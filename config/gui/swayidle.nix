42a069c085d52962e5bc33c3fb9df891137b54c0
fym998 <61316972+fym998@users.noreply.github.com>
2026-05-08 01:26:14 +0800

    fix(swayidle): remove freeze and unfreeze noctalia-shell since it's not managed by systemd anymore

--- config/gui/swayidle.nix
+++ config/gui/swayidle.nix
@@ -8,18 +8,11 @@
     let
       lockScreen = config.lib.genericLinux.getCmd pkgs.swaylock "swaylock -f -F";
       powerOffMonitors = config.lib.genericLinux.getCmd config.programs.niri.package "niri msg action power-off-monitors";
-      freezeShell = config.lib.genericLinux.getCmd pkgs.systemd "systemctl --user freeze noctalia-shell.service";
-      unfreezeShell = config.lib.genericLinux.getCmd pkgs.systemd "systemctl --user thaw noctalia-shell.service";
       sleep = config.lib.genericLinux.getCmd pkgs.systemd "systemctl suspend";
     in
     {
       enable = true;
       timeouts = [
-        {
-          timeout = 900;
-          command = freezeShell;
-          resumeCommand = unfreezeShell;
-        }
         {
           timeout = 900;
           command = powerOffMonitors;
