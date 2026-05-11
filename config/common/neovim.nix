cc4c9d49af7439510330d72b7d843f87f4bd2902
fym998 <61316972+fym998@users.noreply.github.com>
2026-05-11 14:20:19 +0800

    feat(common/neovim): include hidden files in telescope

--- config/common/neovim.nix
+++ config/common/neovim.nix
@@ -100,8 +100,48 @@
             },
           })
 
-          vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-          vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
+          vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files({
+              find_command = {
+                "rg",
+                "--files",
+                "--hidden",
+                "--glob",
+                "!**/.git/**",
+              },
+            })
+          end, { desc = "Find Files" })
+          vim.keymap.set("n", "<leader>fF", function()
+            builtin.find_files({
+              find_command = {
+                "rg",
+                "--files",
+                "--hidden",
+                "--no-ignore",
+                "--glob",
+                "!**/.git/**",
+              },
+            })
+          end, { desc = "Find Files Ignoring Gitignore" })
+          vim.keymap.set("n", "<leader>fg", function()
+            builtin.live_grep({
+              additional_args = {
+                "--hidden",
+                "--glob",
+                "!**/.git/**",
+              },
+            })
+          end, { desc = "Live Grep" })
+          vim.keymap.set("n", "<leader>fG", function()
+            builtin.live_grep({
+              additional_args = {
+                "--hidden",
+                "--no-ignore",
+                "--glob",
+                "!**/.git/**",
+              },
+            })
+          end, { desc = "Live Grep Ignoring Gitignore" })
           vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
           vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Tags" })
           vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
