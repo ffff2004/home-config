349ea8ea7e8b029532551aa0012b4c00932d4f74
fym998 <61316972+fym998@users.noreply.github.com>
2026-05-13 02:12:25 +0800

    fix(common/codex): copy config files during activation

--- config/common/codex/default.nix
+++ config/common/codex/default.nix
@@ -1,9 +1,29 @@
-{ localLib, lib, ... }:
 {
-  home.file = lib.genAttrs' (localLib.lsFileRecursively ./config) (
-    file:
-    lib.nameValuePair ".codex/${lib.removePrefix ((toString ./config) + "/") (toString file)}" {
-      source = localLib.mkSymlinkToSource file;
-    }
+  config,
+  localLib,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  configRoot = ./config;
+in
+{
+  home.activation.copyCodexConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+    lib.concatMapStrings (
+      file:
+      let
+        relativePath = lib.removePrefix ((toString configRoot) + "/") (toString file);
+        target = "${config.home.homeDirectory}/.codex/${relativePath}";
+      in
+      ''
+        run ${pkgs.coreutils}/bin/rm -f ${lib.escapeShellArg target}
+        if [ -x ${lib.escapeShellArg (toString file)} ]; then
+          run ${pkgs.coreutils}/bin/install -Dm755 ${lib.escapeShellArg (toString file)} ${lib.escapeShellArg target}
+        else
+          run ${pkgs.coreutils}/bin/install -Dm644 ${lib.escapeShellArg (toString file)} ${lib.escapeShellArg target}
+        fi
+      ''
+    ) (localLib.lsFileRecursively configRoot)
   );
 }
