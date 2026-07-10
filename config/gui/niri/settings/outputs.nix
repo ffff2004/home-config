34f36fb22a7118637f71b25b9069454b015f7046
ffff2004 <293283756+ffff2004@users.noreply.github.com>
2026-07-10 15:42:16 +0800

    feat(gui/niri): outputs

---
+++ config/gui/niri/settings/outputs.nix
@@ -0,0 +1,15 @@
+{
+  programs.niri.settings.outputs = {
+    eDP-1 = {
+      variable-refresh-rate = true;
+    };
+    DP-1 = {
+      mode = {
+        refresh = 143.988;
+        width = 3840;
+        height = 2160;
+      };
+      variable-refresh-rate = true;
+    };
+  };
+}
