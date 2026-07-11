e444a01ffcc0d91b9c8df3ed6076c7936cf9557c
ffff2004 <293283756+ffff2004@users.noreply.github.com>
2026-07-11 16:47:33 +0800

    feat(rust): set rustup mirror

--- config/common/rust/default.nix
+++ config/common/rust/default.nix
@@ -3,5 +3,9 @@
   home = {
     packages = [ pkgs.rustup ];
     file.".cargo/config.toml".source = localLib.mkSymlinkToSource ./cargo-config.toml;
+    sessionVariables = {
+      RUSTUP_UPDATE_ROOT = "https://mirrors.cernet.edu.cn/rustup/rustup";
+      RUSTUP_DIST_SERVER = "https://mirrors.cernet.edu.cn/rustup";
+    };
   };
 }
