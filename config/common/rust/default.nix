5c00371a768265a6d67e3d6d9656d3b2fd8159a3
ffff2004 <293283756+ffff2004@users.noreply.github.com>
2026-07-12 15:31:52 +0800

    feat(rust): set CARGO_HOME and PATH

--- config/common/rust/default.nix
+++ config/common/rust/default.nix
@@ -1,11 +1,21 @@
-{ pkgs, localLib, ... }:
+{
+  config,
+  pkgs,
+  localLib,
+  ...
+}:
+let
+  cargoHome = "${config.home.homeDirectory}/.cargo";
+in
 {
   home = {
     packages = [ pkgs.rustup ];
-    file.".cargo/config.toml".source = localLib.mkSymlinkToSource ./cargo-config.toml;
+    file."${cargoHome}/config.toml".source = localLib.mkSymlinkToSource ./cargo-config.toml;
     sessionVariables = {
+      CARGO_HOME = cargoHome;
       RUSTUP_UPDATE_ROOT = "https://mirrors.cernet.edu.cn/rustup/rustup";
       RUSTUP_DIST_SERVER = "https://mirrors.cernet.edu.cn/rustup";
     };
+    sessionSearchVariables.PATH = [ "${cargoHome}/bin" ];
   };
 }
