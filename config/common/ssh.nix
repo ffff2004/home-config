eee2dd45679220c601526a6220fec5c382046ad4
ffff2004 <293283756+ffff2004@users.noreply.github.com>
2026-07-18 01:39:06 +0800

    feat(common/ssh): add fujun hosts

--- config/common/ssh.nix
+++ config/common/ssh.nix
@@ -3,7 +3,21 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    settings."*".AddKeysToAgent = "yes";
+    settings = {
+      "*".AddKeysToAgent = "yes";
+
+      fujun = {
+        HostName = "fujun";
+        User = "fujun";
+      };
+
+      fujun-wsl = {
+        HostName = "127.0.0.1";
+        User = "fym";
+        Port = 2222;
+        ProxyJump = "fujun";
+      };
+    };
   };
 
   services.ssh-agent.enable = lib.mkDefault true;
