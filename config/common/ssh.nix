3069921efe8b4633b28458e9c0a1798dc7ab5123
ffff2004 <293283756+ffff2004@users.noreply.github.com>
2026-09-01 13:51:02 +0800

    refactor(ssh): use ts-* name for tailscale

--- config/common/ssh.nix
+++ config/common/ssh.nix
@@ -4,24 +4,31 @@
     enable = true;
     enableDefaultConfig = false;
     settings = {
-      "*".AddKeysToAgent = "yes";
-      "*".ControlMaster = "auto";
-      "*".ControlPersist = "10m";
-      "*".ControlPath = "~/.ssh/cm-%C";
+      "*" = {
+        AddKeysToAgent = "yes";
+        ControlMaster = "auto";
+        ControlPersist = "10m";
+        ControlPath = "~/.ssh/cm-%C";
+      };
 
-      fujun = {
+      # Tailscale
+      "ts-*" = {
+        Compression = "yes";
+      };
+
+      ts-fujun = {
         HostName = "fujun";
         User = "fujun";
       };
 
-      fujun-wsl = {
+      ts-fujun-wsl = {
         HostName = "127.0.0.1";
         User = "fym";
         Port = 2222;
-        ProxyJump = "fujun";
+        ProxyJump = "ts-fujun";
       };
 
-      legion-neoarch-nixcache = {
+      ts-legion-neoarch-nixcache = {
         HostName = "legion-neoarch";
         User = "nixcache";
         IdentityFile = "~/.ssh/id_ed25519-nixcache";
